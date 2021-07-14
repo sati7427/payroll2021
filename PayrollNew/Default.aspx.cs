ca6a4b6e050553e3a1fe0a18ba855eb9a13411e0
Sathish <sati7427@gmail.com>
2021-07-14 13:35:54 +0530

    chamges

--- PayrollNew/Default.aspx.cs
+++ PayrollNew/Default.aspx.cs
@@ -9,20 +9,10 @@ namespace PayrollNew
 {
     public partial class _Default : Page
     {
-       // private readonly Random _random = new Random(); 
         protected void Page_Load(object sender, EventArgs e)
         {
             Response.Redirect("Login/Login.aspx");
-           // txtx.Text = RandomNumber(1000, 9999).ToString();
         }
-        //public int RandomNumber(int min, int max)
-        //{
-        //    return _random.Next(min, max);
-        //}
 
-        //protected void Button1_Click(object sender, EventArgs e)
-        //{
-        //    ClientScript.RegisterStartupScript(this.GetType(), "Pop", "AlertModel();", true);
-        //}  
     }
 }
